d8ed3fc91e2f62531bf6e90382e59b289508ac69
Simeon <mrayendi1@gmail.com>
2024-01-10 11:22:12 -0800

    Created api for register and login

--- core/views.py
+++ core/views.py
@@ -3,7 +3,7 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.permissions import AllowAny
 from django.contrib.auth import authenticate, login
-from core.serializers import RegisterSerializer
+from .serializers import RegisterSerializer
 
 class RegisterView(APIView):
     permission_classes = [AllowAny]
