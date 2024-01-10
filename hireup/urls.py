c98e7afa7d672a70e083f3988e70e4e879a5c097
Simeon <mrayendi1@gmail.com>
2024-01-10 02:23:37 -0800

    fixed bug by install setup tools

--- hireup/urls.py
+++ hireup/urls.py
@@ -15,8 +15,26 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+
+shema_view = get_schema_view (
+    openapi.Info (
+        title = 'Hireup Api',
+        default_version = 'v1',
+        description = 'Api Endpoints for Hireup',
+
+    ),
+    public=True,
+    permission_classes = (permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('swagger/', shema_view.with_ui('swagger', cache_timeout=0), name='shema-swagger-ui'),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', shema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
