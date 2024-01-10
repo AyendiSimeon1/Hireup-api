7b35615e337a0fe2a6a5b78330a397a5f79d88b0
Simeon <mrayendi1@gmail.com>
2024-01-10 03:02:43 -0800

    created serializer.py

---
+++ core/serializers.py
@@ -0,0 +1,34 @@
+from rest_framework import serializers
+from django.contrib.auth import get_user_model
+from allauth.account.adapter import get_adapter
+from allauth.account.utils import setup_user_email
+
+
+User = get_user_model()
+
+class RegisterSerialier(serializers.Serializer):
+    email = serializers.EmailField(required=allauth_settings.EMAIL_REQUIRED_FIELD)
+    username = serializers.CharField(required=True, write_only=True)
+    password1 = serializers.CharField(write_only=True)
+    password2 = serializers.CharField(write_only=True)
+
+
+    def validate_username(self, username):
+        username = get_adapter().clean_username(username)
+        return username
+    
+    def validate_password1(self, password):
+        return get_adapter().clean_username(password)
+
+    def validate(self, data):
+
+        if data['password1'] != data['password2']:
+            raise serializers.ValidationError("The two password fields do not match")
+
+        return data
+    def save(self, request):
+        user = User(username=self.validated_data['username'], email=self.validated_data['email'])
+        user.set_password(self.validated_data['password1'])
+        user.save()
+        setup_user_email(request, user, [])
+        return user
