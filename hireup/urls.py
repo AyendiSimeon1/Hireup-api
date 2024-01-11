3bef3e6ed0039a2ef8dd17c3a29894ad3e6e0520
Simeon <mrayendi1@gmail.com>
2024-01-11 03:43:48 -0800

    Configured urls

--- hireup/urls.py
+++ hireup/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path, include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
-from core.views import RegisterView, LoginView
 
 shema_view = get_schema_view (
     openapi.Info (
@@ -19,9 +18,9 @@ shema_view = get_schema_view (
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/register/', RegisterView.as_view(), name='api_register'),
-    path('api/login/', LoginView.as_view(), name='api_login'),
+   
     path('accounts/', include('allauth.urls')),
+    path('core/', include('core.urls')),
     path('', shema_view.with_ui('swagger', cache_timeout=0), name='shema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', shema_view.without_ui(cache_timeout=0), name='schema-json'),
 ]
