7d17bc5ff4f108f8caf10937e15ffccd13da74ac
Simeon <mrayendi1@gmail.com>
2024-01-11 04:51:00 -0800

    configured urls

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,26 @@
-from django.urls import path
+from django.urls import path, include
 from .views import RegisterView, LoginView, Profile
+from rest_framework.routers import DefaultRouter
+from .views import (
+    PersonalInformationViewSet,
+    EducationViewSet,
+    WorkExperienceViewSet,
+    SkillViewSet,
+    ProjectViewSet,
+)
+
+router = DefaultRouter()
+router.register(r'personal-information', PersonalInformationViewSet)
+router.register(r'education', EducationViewSet)
+router.register(r'work-experience', WorkExperienceViewSet)
+router.register(r'skill', SkillViewSet)
+router.register(r'project', ProjectViewSet)
 
 
 urlpatterns= [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
     path('profile/', Profile.as_view(), name='profile'),
+    path('', include(router.urls)),
 
 ]
