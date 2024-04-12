e8fe4782a10702adfb6347e8b593de9dec8d4af8
Simeon <mrayendi1@gmail.com>
2024-04-12 07:27:10 -0700

    installed jwt

--- hireup/urls.py
+++ hireup/urls.py
@@ -5,6 +5,11 @@ from drf_yasg import openapi
 from rest_framework import permissions
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+
+)
 
 shema_view = get_schema_view (
     openapi.Info (
@@ -25,6 +30,8 @@ urlpatterns = [
     path('core/', include('core.urls')),
     path('', shema_view.with_ui('swagger', cache_timeout=0), name='shema-swagger-ui'),
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', shema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('token/obtain', TokenObtainPairView.as_view(), name='token-obtain'),
+    path('token/refresh', TokenRefreshView.as_view(), name='token-fresh')
 ]
 
 if settings.DEBUG:
